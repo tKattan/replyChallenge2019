210fa8848bf1a687a848ed967e64fd54f1bb35a3
#60705208 <60705208+Seyte@users.noreply.github.com>
2020-09-30 12:48:57 +0200

    output first attempt, create a txt file with the lines

--- Program.cs
+++ Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.IO;
 
-namespace replyChallenge2019
+
+namespace ReplyChallenge
 {
-    class Program
+    class program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            System.Console.WriteLine("Hello Reply Challenge 2019!");
+            string[] lines = { "test 1", "test2", "jsp" };
+            System.Console.WriteLine(generateOutput(lines));
+        }
+        /* string generateOneLine(int x, int y, string line){
+            string line=x.ToString()+" "+y.ToString()+" "+line.ToString();
+            return line;
+        } */
+        
+        static bool generateOutput(string[] lines){
+            using (System.IO.StreamWriter file =
+                new System.IO.StreamWriter(@"./output/output.txt"))
+                foreach (string line in lines){
+                    file.WriteLine(line);
+                }
+            return true;
         }
     }
 }
